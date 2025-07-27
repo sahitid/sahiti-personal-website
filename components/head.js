cc256032689ac8dd83484f6f08080be8f9548bf0
Sahiti Dasari <sahitiaws@gmail.com>
2025-07-26 21:56:08 -0400

    remove all opengraph img

--- components/head.js
+++ components/head.js
@@ -27,13 +27,9 @@ export default function HeadObject({ children }) {
       <meta property="og:type" content="website" />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content={`${url}/api/og`} />
-      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:card" content="summary" />
       <meta name="twitter:site" content={twitter} />
       <meta name="twitter:creator" content={twitter} />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={`${url}/api/og`} />
       {/* Add analytics here */}
       {children}
     </Head>
