c7e3e559fc6fc281bab1db7b51170968b3a53557
Sahiti Dasari <sahitiaws@gmail.com>
2025-12-22 18:17:27 -0500

    revert navbar

--- pages/index.js
+++ pages/index.js
@@ -57,8 +57,7 @@ export default function Home() {
           />
         </div>
 
-        <nav className="absolute right-0 top-0 m-4 sm:m-8 md:m-16 flex flex-col items-end space-y-2 text-[#FF4444] font-gilroy font-bold text-xl sm:text-2xl tracking-wide leading-tight">
-          <span className="nav-link opacity-50 cursor-not-allowed">/home</span>
+        <nav className="absolute right-0 top-0 m-4 sm:m-8 md:m-16 flex flex-col space-y-2 text-[#FF4444] font-gilroy font-bold text-2xl tracking-wide leading-tight">          <span className="nav-link opacity-50 cursor-not-allowed">/home</span>
           <a href="/projects" className="nav-link transition-transform duration-300 hover:scale-105">/projects</a>
           <a href="/photos" className="nav-link transition-transform duration-300 hover:scale-105">/photos</a>
         </nav>
