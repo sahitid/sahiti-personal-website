f5f9a089b76383bcc1d04104f624d3046298c5e7
Sahiti Dasari <sahitiaws@gmail.com>
2026-03-11 18:42:26 -0400

    Revert "fix navbar font"
    This reverts commit 68df699566da86273033dcfa4b908901119deeb5.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 const {nextui} = require('@nextui-org/theme');
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
-  ],
+    "./node_modules/@nextui-org/theme/dist/components/[object Object].js"
+],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
     extend: {
