68df699566da86273033dcfa4b908901119deeb5
Sahiti Dasari <sahitiaws@gmail.com>
2026-03-11 18:40:08 -0400

    fix navbar font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 const {nextui} = require('@nextui-org/theme');
 module.exports = {
   content: [
-    "./node_modules/@nextui-org/theme/dist/components/[object Object].js"
-],
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+  ],
   darkMode: 'class',
   theme: {
     extend: {
